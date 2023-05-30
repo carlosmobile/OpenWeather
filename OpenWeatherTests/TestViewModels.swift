a71c6885e0da39898adcdf9cab17e3937c77ccae
Carlos <revealdottedline@icloud.com>
2023-05-30 19:41:19 +0200

    Add viewmodel unit test

--- OpenWeatherTests/TestViewModels.swift
+++ OpenWeatherTests/TestViewModels.swift
@@ -13,10 +13,35 @@ class TestViewModels: XCTestCase {
     func testWeatherViewModel() throws {
         let weatherViewModel = WeatherViewModel()
         
+        let json = WeatherModelJSON
+        let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+        do {
+            var weatherModel = try JSONDecoder().decode(WeatherModel.self, from: data)
+            weatherModel.city = "Alicante"
+            weatherViewModel.weatherData.value = weatherModel
+        } catch {
+            XCTAssert(false)
+        }
+        
+        weatherViewModel.isNecessaryToShowBottomLocationSheet.value = true
+        weatherViewModel.isLoadingData.value = true
+        weatherViewModel.isUpdateData = true
+        weatherViewModel.footerSheetType = .locationAlert
 
-
-//        XCTAssertEqual(0, true)
-//        XCTAssertNotNil(title)
+        weatherViewModel.checkLocation()
+        weatherViewModel.refreshWeatherData()
+        let internetErrorControl = weatherViewModel.hasInternetConnection()
+        weatherViewModel.setFooterSheetAlertNavigation()
+        let coordinates = weatherViewModel.getLocationCoordinates()
+        weatherViewModel.getLocationPlace(coordinates) { city in
+            XCTAssertNotNil(city)
+        }
+        
+        XCTAssertNotNil(coordinates)
+        XCTAssertNotNil(internetErrorControl)
+        XCTAssertEqual(weatherViewModel.isNecessaryToShowBottomLocationSheet.value, true)
+        XCTAssertEqual(weatherViewModel.isLoadingData.value, true)
+        XCTAssertEqual(weatherViewModel.isUpdateData, true)
+        XCTAssertEqual(weatherViewModel.footerSheetType, .locationAlert)
     }
-
 }
